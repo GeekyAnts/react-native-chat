9dfb89009a2acf8f9902cb451058b689b3f7f704
bhavish <bhavish@geekyants.com>
2017-06-21 11:49:33 +0530

    index file

---
+++ index.js
@@ -0,0 +1,9 @@
+import GeekChat from "./App";
+export { GeekChat };
+
+// import GeekChat, {getToken, setToken } from './App';
+// export {
+//     GeekChat,
+//     getToken,
+//     setToken
+// };
